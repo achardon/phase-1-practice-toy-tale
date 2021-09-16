3db0dbac24cbd7712ee0c41b132aad9c1fdbd633
Alessandra Chardon <achardon@ucla.edu>
2021-09-15 20:17:50 -0400

    rendered cards, added new cards, draft like functionality

--- src/index.js
+++ src/index.js
@@ -13,3 +13,116 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.querySelector('form.add-toy-form').addEventListener('submit', e => {
+  e.preventDefault()
+  addNewToy(e);
+})
+
+//fetch request for toys
+function getToys() {
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(data => {
+    renderToyCard(data);
+  })
+}
+
+//make cards for each toy
+function renderToyCard(data) {
+  //console.log(data)
+  for (let toy in data) {
+    //console.log(data[toy].name)
+    const newToy = document.createElement('div')
+    newToy.className = 'card'
+    document.querySelector('#toy-collection').appendChild(newToy)
+    const h2 = document.createElement('h2')
+    h2.innerText = data[toy].name
+    newToy.appendChild(h2)
+    //debugger;
+    const img = document.createElement('img')
+    img.src = data[toy].image
+    img.className = 'toy-avatar'
+    newToy.appendChild(img)
+    const p = document.createElement('p')
+    p.innerText = `${data[toy].likes} Likes`
+    newToy.appendChild(p)
+    const button = document.createElement('button')
+    button.className = 'like-btn'
+    button.id = data[toy].id
+    button.innerText = 'Like'
+    newToy.appendChild(button)
+    button.addEventListener('click', () => increaseLike(data[toy].likes, button))
+  }
+    //update likes
+    // const likeButtons = document.querySelectorAll('button.like-btn')
+    // likeButtons.forEach(button => {
+    //   button.addEventListener('click', () => increaseLike(button))
+    // })
+  
+}
+
+//increaseLike function with patch request
+function increaseLike(currentLikes, button) {
+  const toyID = button.id
+  //debugger;
+  const updatedLikes = ++currentLikes
+  fetch(`http://localhost:3000/toys/${toyID}`, {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        "likes": updatedLikes
+      })
+    })
+    .then(res => res.json())
+    .then(() => {
+      const div = button.parentNode
+      div.querySelector('p').innerText = `${updatedLikes} Likes`
+    })
+}
+
+  //adding new toy with post request
+function addNewToy (e) {
+    fetch('http://localhost:3000/toys', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        "name": e.target.name.value,
+        "image": e.target.image.value,
+        "likes": 0
+      })
+    })
+    .then(res => res.json())
+    .then((data) => {
+    //console.log(data.likes)
+    const newToy = document.createElement('div')
+      newToy.className = 'card'
+      document.querySelector('#toy-collection').appendChild(newToy)
+      const h2 = document.createElement('h2')
+      h2.innerText = e.target.name.value
+      newToy.appendChild(h2)
+      const img = document.createElement('img')
+      img.src = e.target.image.value
+      img.className = 'toy-avatar'
+      newToy.appendChild(img)
+      const p = document.createElement('p')
+      p.innerText = `0 Likes`
+      newToy.appendChild(p)
+      const button = document.createElement('button')
+      button.className = 'like-btn'
+      button.id = data.id
+      button.innerText = 'Like'
+      newToy.appendChild(button)
+      button.addEventListener('click', () => increaseLike(data.likes, button))
+      })
+}
+
+
+
+getToys();
